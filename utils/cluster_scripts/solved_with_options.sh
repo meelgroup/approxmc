13174f537f53e50a62ccf168743f5f860163dd37
Mate Soos <soos.mate@gmail.com>
2021-01-27 22:40:52 +0100

    New name and updated contents for this script

--- utils/cluster_scripts/solved_with_options.sh
+++ utils/cluster_scripts/solved_with_options.sh
@@ -4,6 +4,7 @@ check="$1"
 FILE="20180321_110706599_p_cnf_320_1120.cnf"
 FILE="001-80-12-sc2014.cnf"
 FILE="01A-1.cnf.gz.no_w.cnf"
+FILE="01A-1.cnf.gz.no_w.cnf"
 
 rm out
 for d in `ls | grep $check`; do
@@ -12,8 +13,10 @@ for d in `ls | grep $check`; do
     name=`xzgrep "Command being" $d/${FILE}.gz.timeout.xz`
     numsolved=`wc -l $d/solved.csv | awk '{print $1}'`
     numALL=`wc -l $d/allFiles.csv  | awk '{print $1}'`
+    #c Arjun SHA revision 8885476de181e6efe9ab3038ded86ed0bcd9cfec
+    revArj=`xzgrep -i "Arjun.*revision" $d/${FILE}.gz.out.xz | awk '{print $5}' | cut -c1-7`
     revCMS=`xzgrep -i "CMS.*revision" $d/${FILE}.gz.out.xz | awk '{print $5}' | cut -c1-7`
     revApp=`xzgrep -i "AppMC.*revision" $d/${FILE}.gz.out.xz | awk '{print $5}' | cut -c1-7`
-    echo "$PAR2 $d $numsolved $numALL $revApp $revCMS $name" >> out
+    echo "$PAR2 $d $numsolved $numALL $revApp $revCMS $revArj $name" >> out
 done
 sed "s/${FILE}.*//" out | sed "s/Command.*time.*approxmc//"  | sed "s/.solved.csv//" |                 sed "s/ *Command being timed.*approxmc//" | sed "s/\t/ /g" | sed "s/\t/ /g" | sort -n
