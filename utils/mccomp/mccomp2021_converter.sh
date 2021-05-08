7a54e5d08966f31ee1dc7badf9717cf675af9670
Mate Soos <soos.mate@gmail.com>
2021-05-08 23:08:52 +0200

    Using TMPDIR

--- utils/mccomp/mccomp2021_converter.sh
+++ utils/mccomp/mccomp2021_converter.sh
@@ -4,13 +4,24 @@ file=$1
 mc=`grep "^c t " $file`
 echo "c o found header: $mc"
 
+if [ -z ${TMPDIR+x} ];
+then
+    TMPDIR="."
+    echo "c o TMP dir not defined, setting it to $TMPDIR"
+else
+    echo "c o TMP dir defined: $TMPDIR"
+fi
+
+rm -f "$TMPDIR/ind.txt" "$TMPDIR/sol.txt" "$TMPDIR/clean_file.txt"
+
+
 if [[ "$mc" = "c t mc" ]]; then
     echo "c o this is a regular model counting file"
-    grep -v "^c" $file |  ./approxmc > sol.txt
-    sed -E "s/^(.)/c o \1/" sol.txt
+    grep -v "^c" $file |  ./approxmc > "$TMPDIR/sol.txt"
+    sed -E "s/^(.)/c o \1/" "$TMPDIR/sol.txt"
 
-    sat=`grep "^s .*SATIS" sol.txt`
-    count=`grep "^s mc" sol.txt | awk '{print $3}'`
+    sat=`grep "^s .*SATIS" "$TMPDIR/sol.txt"`
+    count=`grep "^s mc" "$TMPDIR/sol.txt" | awk '{print $3}'`
     log_10_count=`echo "scale=15; l($count)/l(10)" | bc -l `
 
     echo $sat
@@ -20,13 +31,13 @@ if [[ "$mc" = "c t mc" ]]; then
 
 elif [[ "$mc" = "c t pmc" ]]; then
     echo "c o this is a projected model counting file"
-    grep "c p show" $file | sed -E "s/c p show (.*)/c ind \1 0/" > ind.txt
-    grep -v "^c" $file >> clean_file.txt
-    cat clean_file.txt ind.txt | ./approxmc  > sol.txt
-    sed -E "s/^(.)/c o \1/" sol.txt
+    grep "c p show" $file | sed -E "s/c p show (.*)/c ind \1 0/" > "$TMPDIR/ind.txt"
+    grep -v "^c" $file > "$TMPDIR/clean_file.txt"
+    cat "$TMPDIR/clean_file.txt" "$TMPDIR/ind.txt" | ./approxmc  > "$TMPDIR/sol.txt"
+    sed -E "s/^(.)/c o \1/" "$TMPDIR/sol.txt"
 
-    sat=`grep "^s .*SATIS" sol.txt`
-    count=`grep "^s mc" sol.txt | awk '{print $3}'`
+    sat=`grep "^s .*SATIS" "$TMPDIR/sol.txt"`
+    count=`grep "^s mc" "$TMPDIR/sol.txt" | awk '{print $3}'`
     log_10_count=`echo "scale=15; l($count)/l(10)" | bc -l `
 
     echo $sat
@@ -42,3 +53,5 @@ else
     echo "ERROR: Header not found, this is not an MCComp 2021 file!"
     exit -1
 fi
+
+rm -f "$TMPDIR/ind.txt" "$TMPDIR/sol.txt" "$TMPDIR/clean_file.txt"
