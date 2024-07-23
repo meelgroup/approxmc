d5ebaca1245932adef360879a9c1eafa695cf778
Mate Soos <soos.mate@gmail.com>
2024-07-23 23:20:58 +0200

    No tests in static mode

--- scripts/build_scripts/build_static.sh
+++ scripts/build_scripts/build_static.sh
@@ -4,5 +4,4 @@ set -e
 
 rm -rf lib* Test* tests* include tests scalmc* CM* cmake*
 cmake -DSTATICCOMPILE=ON ..
-make -j6
-make test
+make -j12
--- scripts/build_scripts/build_static_release.sh
+++ scripts/build_scripts/build_static_release.sh
@@ -4,5 +4,4 @@ set -e
 
 rm -rf lib* Test* tests* include tests scalmc* CM* cmake*
 cmake -DCMAKE_BUILD_TYPE=Release -DSTATICCOMPILE=ON ..
-make -j6
-make test
+make -j12
