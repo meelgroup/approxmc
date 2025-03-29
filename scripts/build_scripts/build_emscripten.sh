86f7dc0c0eb0ce37c358aa94070763f4fc8e1559
Mate Soos <soos.mate@gmail.com>
2025-03-29 16:34:55 +0100

    Making emscripten work

---
+++ scripts/build_scripts/build_emscripten.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+rm -rf lib* Test* tests* include tests scalmc* CM* cmake* approxmc* apx-src
+emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
+emmake make -j16
+emmake make install
