5c6a00d64e991b6a420aeb83f975e990eeb067f9
Mate Soos <soos.mate@gmail.com>
2026-04-15 11:02:42 +0200

    Fix emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -12,7 +12,15 @@ rm -rf approxmc*
 rm -rf apx-src
 rm -rf deps
 rm -rf _deps
-emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
+emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL \
+    -DBUILD_SHARED_LIBS=OFF \
+    -Dcadical_DIR="${SOLVERS_DIR}/cadical/build" \
+    -Dcadiback_DIR="${SOLVERS_DIR}/cadiback/build" \
+    -Dcryptominisat5_DIR="${SOLVERS_DIR}/cryptominisat/build" \
+    -Dsbva_DIR="${SOLVERS_DIR}/sbva/build" \
+    -Dtreedecomp_DIR="${SOLVERS_DIR}/treedecomp/build" \
+    -Darjun_DIR="${SOLVERS_DIR}/arjun/build" \
+    ..
 emmake make -j$(nproc)
 emmake make install
 cp approxmc.wasm ../html
